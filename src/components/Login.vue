55e02b8832174c9fac3a2ee8f6a86754a638cdef
Sunshine-sun <1396610542@qq.com>
2019-10-24 16:52:57 +0800

    登录功能完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,104 @@
 <template>
-<div class="login">
-  我是登录组件
-</div>
+  <div class="login">
+    <el-form ref="form" status-icon :rules="rules" :model="form" label-width="80px">
+      <img class="logo" src="../assets/1.jpg" alt />
+      <el-form-item label="用户名" prop="username">
+        <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="password">
+        <el-input type="password" placeholder="请输入密码" v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="login" class="loginBtn" type="primary">登录</el-button>
+        <el-button @click="reset">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: ['change', 'blur'] },
+          { min: 3, max: 12, message: '用户名的长度必须是3-12位', trigger: ['change', 'blur'] }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: ['change', 'blur'] },
+          { min: 3, max: 12, message: '密码的长度必须是3-12位', trigger: ['change', 'blur'] }
+        ]
+      }
+    }
+  },
+  methods: {
+    reset () {
+      this.$refs.form.resetFields()
+    },
+    login () {
+      this.$refs.form.validate(isValid => {
+        if (!isValid) return
+        console.log(1111)
+        axios.post('http://localhost:8888/api/private/v1/login', this.form).then(res => {
+          const { meta, data } = res.data
+          if (meta.status === 200) {
+            // console.log(meta.msg)
+            localStorage.setItem('token', data.token)
+            this.$message({
+              type: 'success',
+              message: meta.msg,
+              duration: 1000
+            })
+            this.$router.push('/index')
+          } else {
+            this.$message({
+              type: 'error',
+              message: meta.msg,
+              duration: 1000
+            })
+          }
+        })
+      })
+    }
 
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss">
+.login {
+  background-color: #2d434c;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  .el-form {
+    background-color: #fff;
+    width: 400px;
+    padding: 20px;
+    padding-top: 70px;
+    border-radius: 20px;
+    margin: 0 auto;
+    margin-top: 200px;
+    position: relative;
+    .logo {
+      width: 120px;
+      height: 120px;
+      position: absolute;
+      left: 50%;
+      transform: translateX(-50%);
+      top: -70px;
+      border-radius: 50%;
+      border: 5px solid #fff;
+    }
+    .loginBtn {
+      margin-right: 70px;
+    }
+  }
+}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Index from '../components/Index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  { path: '/', component: Login }
+  { path: '/', redirect: '/index' },
+  { path: '/index', component: Index },
+  { path: '/login', component: Login }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (to.path === '/login' || token) {
+    next()
+  } else {
+    next('/login')
+  }
 })
 
 export default router
